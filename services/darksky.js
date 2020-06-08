765442206cf4234156b21b6f517dcf6d462fd6e0
hackagon <phonghiavan@gmail.com>
2020-06-08 19:13:00 +0700

    refactor darksky api

--- services/darksky.js
+++ services/darksky.js
@@ -1,3 +1,24 @@
-const callGeoCodeAPI = () => {
+const request = require("request"); // ES5
 
+/**
+ * @todo   call darksky API
+ * @param {*} lat latitude 
+ * @param {*} lng longitude
+ * @param {*} callback (err, res)
+ */
+const callDarkSkyAPI = (lat, lng, callback) => {
+  request({
+    url: `https://api.darksky.net/forecast/b8164e69c9f7fbc654f20d2d6381d1fc/${lat},${lng}`,
+    json: true
+  }, (err, res, body) => {
+    // error => Cac ban tu lam
+
+    // result
+    callback(null, {
+      summary: body.currently.summary,
+      temperature: body.currently.temperature
+    })
+  })
 }
+
+module.exports.callDarkSkyAPI = callDarkSkyAPI;
